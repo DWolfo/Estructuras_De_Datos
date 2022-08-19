2a94bddac4c25e84d6fd79fa97f4268c713a4fb2
DWolfo <100615589+DWolfo@users.noreply.github.com>
2022-08-19 09:24:50 -0600

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW/src/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,25 @@
+
+package eva1_2_stackoverflow;
+
+/**
+ *
+ * @author Daniel
+ */
+public class EVA1_2_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        A();
+    }
+    
+}
