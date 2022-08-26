2c3bcfcc467d0494f7c3671d0d8635d45162971d
DWolfo <100615589+DWolfo@users.noreply.github.com>
2022-08-26 09:47:46 -0600

    Add files via upload

---
+++ EVA1_9_ARREGLOS_MULTIDIM/src/eva1_9_arreglos_multidim/EVA1_9_ARREGLOS_MULTIDIM.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_arreglos_multidim;
+
+/**
+ *
+ * @author Daniel
+ */
+public class EVA1_9_ARREGLOS_MULTIDIM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] matrizDatos = new int[3][5];
+        System.out.println("FILAS :  "  + matrizDatos.length);
+        for (int i = 0; i < matrizDatos.length; i++) {
+            System.out.println("COLUMNAS: " + matrizDatos);
+            for (int j = 0; j < matrizDatos[i].length; j++) {
+                
+            }
+            
+        }
+    }
+    
+}
