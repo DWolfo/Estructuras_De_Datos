2ae99212d3e9bff2dfa5e8aea8b5606bef974015
DWolfo <100615589+DWolfo@users.noreply.github.com>
2022-12-13 09:27:50 -0600

    Add files via upload

---
+++ EVA3_1_ORDENAMIENTOS/src/eva3_1_ordenamientos/EVA3_1_ORDENAMIENTOS.java
@@ -0,0 +1,15 @@
+
+package eva3_1_ordenamientos;
+
+/**
+ *
+ * @author Daniel
+ */
+public class EVA3_1_ORDENAMIENTOS {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
